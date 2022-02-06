1adc15028c591436667fde7eee5aaa0b8c4d9448
abdelrhman youssef <abderlrhmanyoussef@gmail.com>
2022-02-06 10:56:56 +0200

    Add Zoom Page

---
+++ IOS Task Trend/IOS Task Trend/Module/Image Zoomed/ViewController/ImageZoomedViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ImageZoomedViewController.swift
+
+//
+//  Created by Apple on 19/01/2021.
+
+//
+
+import UIKit
+import SDWebImage
+class ImageZoomedViewController: UIViewController{
+
+    var imageURL: String?
+   
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.maximumZoomScale = 4
+        scrollView.minimumZoomScale = 1
+        navigationController?.navigationBar.isHidden = false
+
+        scrollView.delegate = self
+        // Do any additional setup after loading the view.
+        if let imageURL = self.imageURL {
+            
+          let url = URL(string: imageURL)
+            imageView.sd_setImage(with: url, completed: nil)
+        }
+    }
+
+}
+extension ImageZoomedViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale > 1 {
+            if let image = imageView.image {
+                let ratioW = imageView.frame.width / image.size.width
+                let ratioH = imageView.frame.height / image.size.height
+                
+                let ratio = ratioW < ratioH ? ratioW : ratioH
+                let newWidth = image.size.width * ratio
+                let newHeight = image.size.height * ratio
+                let conditionLeft = newWidth*scrollView.zoomScale > imageView.frame.width
+                let left = 0.5 * (conditionLeft ? newWidth - imageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+                let conditioTop = newHeight*scrollView.zoomScale > imageView.frame.height
+                
+                let top = 0.5 * (conditioTop ? newHeight - imageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+                
+            }
+        } else {
+            scrollView.contentInset = .zero
+        }
+    }
+}
